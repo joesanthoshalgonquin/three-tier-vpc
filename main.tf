9723e91d540ff3340138677996165e00014bd04b
joesanthoshalgonquin <sant0232@algonquinlive.com>
2024-06-03 09:47:56 -0400

    Edited NAT Gateway tag

--- main.tf
+++ main.tf
@@ -1,94 +1,94 @@
 # VPC
 resource "aws_vpc" "sysops_vpc" {
-    cidr_block = "10.99.0.0/16"
-    instance_tenancy = "default"
-    
-    tags = {
-        Name = "SysOpsVPC"
-    }
+  cidr_block       = "10.99.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "SysOpsVPC"
+  }
 }
 
 # Subnets
 
 # First DMZ Layer
 resource "aws_subnet" "dmz1_public" {
-    vpc_id = aws_vpc.sysops_vpc.id
-    cidr_block = "10.99.1.0/24"
-    availability_zone = "us-east-1a"
-    
-    tags = {
-        Name = "DMZ1public"
-    }
+  vpc_id            = aws_vpc.sysops_vpc.id
+  cidr_block        = "10.99.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "DMZ1public"
+  }
 }
 
 # Second DMZ Layer
 resource "aws_subnet" "dmz2_public" {
-    vpc_id = aws_vpc.sysops_vpc.id
-    cidr_block = "10.99.2.0/24"
-    availability_zone = "us-east-1b"
-    
-    tags = {
-        Name = "DMZ2public"
-    }
+  vpc_id            = aws_vpc.sysops_vpc.id
+  cidr_block        = "10.99.2.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "DMZ2public"
+  }
 }
 
 # First App Layer
 resource "aws_subnet" "applayer1_private" {
-    vpc_id = aws_vpc.sysops_vpc.id
-    cidr_block = "10.99.11.0/24"
-    availability_zone = "us-east-1a"
-    
-    tags = {
-        Name = "AppLayer1private"
-    }
+  vpc_id            = aws_vpc.sysops_vpc.id
+  cidr_block        = "10.99.11.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "AppLayer1private"
+  }
 }
 
 # Second App Layer
 resource "aws_subnet" "applayer2_private" {
-    vpc_id = aws_vpc.sysops_vpc.id
-    cidr_block = "10.99.12.0/24"
-    availability_zone = "us-east-1b"
-    
-    tags = {
-        Name = "AppLayer2private"
-    }
+  vpc_id            = aws_vpc.sysops_vpc.id
+  cidr_block        = "10.99.12.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "AppLayer2private"
+  }
 }
 
 # First Database Layer
 resource "aws_subnet" "dbplayer1_private" {
-    vpc_id = aws_vpc.sysops_vpc.id
-    cidr_block = "10.99.21.0/24"
-    availability_zone = "us-east-1a"
-    
-    tags = {
-        Name = "DBLayer1private"
-    }
+  vpc_id            = aws_vpc.sysops_vpc.id
+  cidr_block        = "10.99.21.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "DBLayer1private"
+  }
 }
 
 # Second Database Layer
 resource "aws_subnet" "dbplayer2_private" {
-    vpc_id = aws_vpc.sysops_vpc.id
-    cidr_block = "10.99.22.0/24"
-    availability_zone = "us-east-1b"
-    
-    tags = {
-        Name = "DBLayer2private"
-    }
+  vpc_id            = aws_vpc.sysops_vpc.id
+  cidr_block        = "10.99.22.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "DBLayer2private"
+  }
 }
 
 resource "aws_internet_gateway" "igw" {
-    vpc_id = aws_vpc.sysops_vpc.id
+  vpc_id = aws_vpc.sysops_vpc.id
 
-    tags = {
-        Name = "IGW"
-    }
+  tags = {
+    Name = "IGW"
+  }
 }
 
 resource "aws_nat_gateway" "ngw" {
-    subnet_id = aws_subnet.dmz2_public.id
-    connectivity_type = "public"
-    
-    tags = {
-        Name = 'NGW'
-    }
+  subnet_id         = aws_subnet.dmz2_public.id
+  connectivity_type = "public"
+
+  tags = {
+    Name = "NGW"
+  }
 }
